e37864baf77c0bc9f495d29a5d2d840c972d5924
Kim Bäckström <kim.backstrom@helsinki.fi>
2015-03-14 22:23:20 +0200

    Add comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
-        char[] string = {'h', 'e', 'l', 'l', 'o', 'h', 'e', 'l', 'l', 'o'};
-        Arrays.sort(string); // eehhlllloo
-        PriorityQueue<Node> queue = new PriorityQueue();
+        char[] string = {'h', 'e', 'l', 'l', 'o', 'h', 'e', 'l', 'l', 'o'}; // TODO: Read from file
+        Arrays.sort(string); // TODO: Replace with own code
+        PriorityQueue<Node> queue = new PriorityQueue<Node>(); // TODO: Replace with own code
         Node node = new Node(string[0]);
-        for (int i = 1 ; i < string.length ; i++) {
+        for (int i = 1 ; i < string.length ; i++) { // Create leaf nodes
             if (node.c!=string[i]) {
                 queue.add(node);
                 node = new Node(string[i]);
@@ -18,7 +18,7 @@ public class Main {
         queue.add(node);
 
         while (queue.size() > 1) {
-            queue.add(new Node(queue.poll(), queue.poll()));
+            queue.add(new Node(queue.poll(), queue.poll())); // Construct tree
         }
     }
 
