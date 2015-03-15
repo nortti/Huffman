0bfb65a66c2f7369400264cc9b6b7f1ffd8ed9e3
Kim Bäckström <kim.backstrom@helsinki.fi>
2015-03-15 14:12:50 +0200

    Read files and use args, code cleaning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,30 @@
 import java.util.PriorityQueue;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.io.*;
 
 public class Main {
-    private static HashMap<Character, String> map = new HashMap<Character, String>();
-    private static String treeString = "";
+    private static FileInputStream fileInput;
     private static int[] charFreqs = new int[128];
-    public static void main (String[] args) {
-        char[] string = {'h', 'e', 'l', 'l', 'o', 'h', 'e', 'l', 'l', 'o'};
-        PriorityQueue<Node> queue = new PriorityQueue<Node>(); // TODO: Replace with own code
-        for (int i = 0 ; i < string.length ; i++) {
-            charFreqs[string[i]]++;
+    private static String[] codeTable = new String[128];
+    private static String tree = "";
+
+    public static void main (String[] args) throws IOException {
+        fileInput = new FileInputStream("src/main/resources/" + args[0]);
+        if (args[1].equals("-d")) {
+            decode();
+        } else {
+            encode();
         }
-        for (int i  = 0 ; i < charFreqs.length ; i++ ) {
+    }
+
+    public static void encode() throws IOException {
+        PriorityQueue<Node> queue = new PriorityQueue<Node>(); // TODO: Replace with own code
+        int r;
+        String input = "";
+        while ((r = fileInput.read()) != -1) {
+            input += (char) r;
+            charFreqs[r]++;
+        }
+        for (int i = 0 ; i < charFreqs.length ; i++) {
             if (charFreqs[i]>0) {
                 queue.add(new Node((char) i, charFreqs[i]));
             }
@@ -21,22 +33,27 @@ public class Main {
             queue.add(new Node(queue.poll(), queue.poll())); // Construct tree
         }
         Node root = queue.poll();
-        map(root, "");
-        System.out.print(map);
-        System.out.println(treeString);
+        createCodes(root, "");
+        String output = tree + ' ';
+        for (int i = 0; i<input.length() ; i++) output+="."+codeTable[input.charAt(i)]+".";
+        System.out.print(output);
     }
 
-    public static void map(Node node, String code) {
-        if (node.leftChild==null && node.rightChild == null) {
-            map.put(node.c, code);
-            treeString += '1' + "" + node.c;
+    public static void decode() {
+    }
+
+    public static void createCodes(Node node, String code) {
+        if (node.leftChild==null && node.rightChild==null) {
+            codeTable[node.c] = code;
+            tree += "1" + node.c;
         } else {
-            treeString += '0';
-            map(node.leftChild, code + '1');
-            map(node.rightChild, code + '0');
+            tree += '0';
+            createCodes(node.leftChild, code + '0');
+            createCodes(node.rightChild, code + '1');
         }
-
     }
+
+
 
     public static class Node implements Comparable<Node> {
         char c;
