f1d1882f2cc53e658bcec2542d6416f6da0c79c1
Kim Bäckström <kim.backstrom@helsinki.fi>
2015-03-16 10:56:00 +0200

    Work on outputting encoding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 import java.util.PriorityQueue;
 import java.io.*;
+import java.*;
 
 public class Main {
-    private static FileInputStream fileInput;
+    private static FileInputStream in;
+    private static FileOutputStream out;
     private static int[] charFreqs = new int[128];
     private static String[] codeTable = new String[128];
     private static String tree = "";
+    private static byte currByte;
+    private static int pos;
 
     public static void main (String[] args) throws IOException {
-        fileInput = new FileInputStream("src/main/resources/" + args[0]);
-        if (args[1].equals("-d")) {
+        in = new FileInputStream("src/main/resources/" + args[0]);
+        if (args.length > 1 && args[1].equals("-d")) {
             decode();
         } else {
             encode();
@@ -20,7 +24,7 @@ public class Main {
         PriorityQueue<Node> queue = new PriorityQueue<Node>(); // TODO: Replace with own code
         int r;
         String input = "";
-        while ((r = fileInput.read()) != -1) {
+        while ((r = in.read()) != -1) {
             input += (char) r;
             charFreqs[r]++;
         }
@@ -33,27 +37,42 @@ public class Main {
             queue.add(new Node(queue.poll(), queue.poll())); // Construct tree
         }
         Node root = queue.poll();
+        out  = new FileOutputStream("src/main/resources/test");
         createCodes(root, "");
+
         String output = tree + ' ';
-        for (int i = 0; i<input.length() ; i++) output+="."+codeTable[input.charAt(i)]+".";
-        System.out.print(output);
+        for (int i = 0; i<input.length() ; i++) output+=codeTable[input.charAt(i)];
+        // System.out.print(output);
     }
 
     public static void decode() {
+
     }
 
-    public static void createCodes(Node node, String code) {
+    public static void createCodes(Node node, String code) throws IOException {
         if (node.leftChild==null && node.rightChild==null) {
             codeTable[node.c] = code;
-            tree += "1" + node.c;
+            write(true);
+            // tree += "1" + Integer.toString(node.c, 0xFF);
         } else {
-            tree += '0';
+            write(false);
+            // tree += '0';
             createCodes(node.leftChild, code + '0');
             createCodes(node.rightChild, code + '1');
         }
     }
 
-
+    public static void write(boolean isOn) throws IOException {
+        if (isOn) {
+            currByte += Math.pow(2, pos);
+        }
+        pos++;
+        if (pos == 7) {
+            out.write(currByte);
+            pos = 0;
+            currByte = 0;
+        }
+    }
 
     public static class Node implements Comparable<Node> {
         char c;
@@ -77,5 +96,3 @@ public class Main {
         }
     }
 }
-
-
