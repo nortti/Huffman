8a46e6a18a2784dc0891b3825206e4260a58b607
Kim Bäckström <kim.backstrom@helsinki.fi>
2015-03-17 17:50:29 +0200

    Give output file from main

--- src/main/java/huffman/Huffman.java
+++ src/main/java/huffman/Huffman.java
@@ -1,18 +1,20 @@
 package huffman;
 
-import huffman.tools.Compresser;
-import java.io.File;
-import java.io.IOException;
+import huffman.compression.Compresser;
+import java.io.*;
 
 /**
- * The main class from where we can either compress or decompress a given file.
+ * The main class which specifies an input and an output file for compression  or decompression,
+ * depending on user input (to be implemented).
  */
 public class Huffman {
 
     public static void main (String[] args) throws IOException {
-        String path = "src/main/resources/Example.txt";
-        File file = new File(path);
+        String inputPath = "src/main/resources/Example.txt";
+        String outputPath = inputPath + ".compressed";
+        File inputFile = new File(inputPath);
+        File outputFile = new File(outputPath);
         Compresser compresser = new Compresser();
-        compresser.generate(file);
+        compresser.generateCompressed(inputFile, outputFile);
     }
 }
