c3268a475547bbfecf7d6b4fa5421c797424af96
Kim Bäckström <kim.backstrom@helsinki.fi>
2015-03-27 17:03:50 +0200

    Tests for FileConverter

--- src/test/java/huffman/io/FileConverterTest.java
+++ src/test/java/huffman/io/FileConverterTest.java
@@ -1,25 +1,64 @@
 package huffman.io;
 
-import static org.junit.Test;
+import huffman.converting.DataConverter;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileConverterTest {
 
+    String path = "src/test/resources/test";
+    File file = new File(path);
     FileConverter fileConverter = new FileConverter();
+    DataConverter dataConverterStub = new DataConverterStub();
+
+    public class DataConverterStub implements DataConverter {
+        @Override
+        public byte[] convert(byte[] data) {
+           return new byte[] { (byte)64 };
+        }
+        @Override
+        public String newPath(String path) {
+            return path + ".test";
+        }
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        file.createNewFile();
+        fileConverter.convert(file, dataConverterStub);
+    }
 
     @Test
     public void OldFileIsDeleted() {
+        assertFalse(file.exists());
     }
 
     @Test
     public void newFileIsCreated() {
+        File dir = new File("src/test/resources/");
+        assertTrue(dir.list().length > 0);
     }
 
     @Test
     public void newFileHasCorrectExtension() {
+        assertTrue(new File(path + ".test").exists());
     }
 
     @Test
-    public void newFileHasCorrectData() {
+    public void newFileHasCorrectData() throws IOException {
+    byte[] data = Files.readAllBytes(Paths.get(path + ".test"));
+    assertArrayEquals(data, new byte[] { (byte)64 });
+    }
+
+    @After
+    public void tearDown() {
+        new File(path + ".test").delete();
     }
 
 }
